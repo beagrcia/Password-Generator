1ffaf27c1ebc436044e7600fa235022f98d90c01
Bea Garcia <113351014+beagrcia@users.noreply.github.com>
2022-10-29 05:20:23 +0800

    V2
    Added slider
    More responsive. Passwords will generate new passwords once button is clicked.

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+
+
 let firstEl = document.getElementById("firstPassword")
 let secondEl = document.getElementById("secondPassword")
 let firstPassword = []
 let secondPassword = []
-let getLength = document.getElementById("passLength")
+let randomChar1 = ""
+let randomChar2 = ""
+let slider = document.getElementById("myRange")
+let output = document.getElementById("value")
 
+output.innerHTML = slider.value;
 
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
-function getPassword(){
+function getPassword() {
+    let firstPassword = ""
+    let secondPassword = ""
     
-    let passCode = Math.floor(Math.random() * characters.length)
-    
-    for (let i = 0; i < characters.length; i++)
-    firstEl.textContent = firstPassword.join("")
-    {
-        if (firstPassword.length < passLength.value) {
-            firstPassword.push(characters[passCode])
-            return getPassword()
-            console.log(firstPassword)
-        } else if (secondPassword.length < passLength.value) {
-            secondPassword.push(characters[passCode])
-            secondEl.textContent = secondPassword.join("")
-            return getPassword()
-            console.log(firstPassword)
-            
-        }
+    for (let i = 0; i < slider.value; i++){
+        let randomIndex1 = Math.floor(Math.random() * characters.length)
+        randomChar1 = characters[randomIndex1]
+        firstPassword += randomChar1
+        firstEl.textContent = firstPassword
         
-        
-        } 
+        let randomIndex2 = Math.floor(Math.random() * characters.length)
+        randomChar2 = characters[randomIndex2]
+        secondPassword += randomChar2
+        secondEl.textContent = secondPassword
         
     }
-
     
+}
+
